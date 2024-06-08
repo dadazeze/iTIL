cd77811d9d87fe1a6e414f5aa4eb5a966788949d
DavinaWorld <kdbj12003@gmail.com>
2024-06-08 16:59:51 +0900

    Feat: 공통 컬러칩 추가

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,10 +1,11 @@
-import BottomNavBarUI from '@/components/common/BottomNavBarUi';
-import { MenubarUI } from '@/components/common/MenuBarUI';
+import BottomNavBarUI from "@/components/common/BottomNavBarUi";
+import { MenubarUI } from "@/components/common/MenuBarUI";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <MenubarUI />
+      <div className="bg-required">testtesttesttesttest</div>
       {children}
       <BottomNavBarUI />
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,53 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ['class'],
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: '',
+  prefix: "",
   theme: {
+    colors: {
+      level: {
+        // 게시글 개수에 따른 색상
+        100: "#D7FEC0", // ex) bg-level-100
+        200: "#92F05B",
+        300: "#409210",
+      },
+      social: {
+        100: "#2DE2D7",
+        200: "#28D0C6", // 소셜 링크 버튼
+      },
+      required: "#F2501D", // 소셜 링크 버튼
+    },
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        '2xl': '1400px',
+        "2xl": "1400px",
       },
     },
     extend: {
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
