ca2f908ea73e0f082ea3c9c7d7cf7698b0276754
DavinaWorld <kdbj12003@gmail.com>
2024-06-10 22:06:22 +0900

    Chore: 불필요한 코드 제거

--- src/components/common/PostModalUI.tsx
+++ src/components/common/PostModalUI.tsx
@@ -3,8 +3,8 @@ import {
   DialogContent,
   DialogTitle,
   DialogDescription,
-  DialogTrigger,
 } from "@radix-ui/react-dialog";
+import { MenubarUI } from "./MenuBarUI";
 
 type Props = {
   open: boolean;
@@ -14,18 +14,31 @@ export default function PostModalUI(props: Props) {
   const { open, close } = props;
   return (
     <Dialog open={open} onOpenChange={close}>
-      <DialogTrigger asChild>
-        <button className="btn">Open Dialog</button>
-      </DialogTrigger>
       <DialogContent className="fixed inset-0 flex items-center justify-center z-[60] bg-white">
         <div className="w-full h-full p-4 flex flex-col">
-          <DialogTitle className="text-lg font-bold mb-4">
-            Full Screen Dialog
+          <MenubarUI />
+          <DialogTitle className="text-lg font-bold mb-4 mt-[24]">
+            <div className="border-b-2">
+              <input
+                type="text"
+                placeholder="제목을 입력하세요"
+                className="py-2 px-5 outline-none font-normal"
+              />
+            </div>
+            <div className="border-b-2  mt-5">
+              <input
+                type="text"
+                placeholder="#대표 키워드 (i.g #Nextjs)"
+                className="py-2 px-5 outline-none font-normal"
+              />
+            </div>
           </DialogTitle>
           <DialogDescription className="flex-1 mb-4">
-            This is a full screen dialog.
+            <textarea
+              className="w-full h-full p-4 outline-none resize-none"
+              placeholder="오늘 공부한 내용을 작성하세요. 단 한줄도 괜찮아요 :)"
+            />
           </DialogDescription>
-          {/* 여기에 추가적인 콘텐츠를 넣으세요 */}
           <button className="self-end btn" onClick={() => close()}>
             Close
           </button>
