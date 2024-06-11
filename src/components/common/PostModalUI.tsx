7c062d686dffe93a82f0a502b5949327e062925d
Da <128560714+DavinaWorld@users.noreply.github.com>
2024-06-11 13:01:20 +0900

    Feat: 글쓰기 에디터 라이브러리 추가

--- src/components/common/PostModalUI.tsx
+++ src/components/common/PostModalUI.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -5,6 +7,9 @@ import {
   DialogDescription,
 } from "@radix-ui/react-dialog";
 import { MenubarUI } from "./MenuBarUI";
+import ReactQuill from "react-quill";
+import { useState } from "react";
+import TextEditor from "@/app/_components/TextEditor";
 
 type Props = {
   open: boolean;
@@ -23,22 +28,23 @@ export default function PostModalUI(props: Props) {
                 <input
                   type="text"
                   placeholder="제목을 입력하세요"
-                  className="py-2 px-1 outline-none font-normal w-full"
+                  className="py-2 px-1 outline-none font-normal w-full text-sm"
                 />
               </div>
               <div className="border-b-2  mt-5">
                 <input
                   type="text"
                   placeholder="#대표 키워드 (i.g #Nextjs)"
-                  className="py-2 px-1 outline-none font-normal w-full "
+                  className="py-2 px-1 outline-none font-normal w-full text-sm"
                 />
               </div>
             </DialogTitle>
             <DialogDescription className="flex-1 mb-4">
-              <textarea
-                className="w-full h-full py-4 px-1 outline-none resize-none"
+              {/* <textarea
+                className="w-full h-full py-4 px-1 outline-none resize-none text-sm"
                 placeholder="오늘 공부한 내용을 작성하세요. 단 한줄도 괜찮아요 :)"
-              />
+              /> */}
+              <TextEditor />
             </DialogDescription>
             <button className="self-end btn" onClick={() => close()}>
               Close
