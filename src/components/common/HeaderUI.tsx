08dbf337c7f033ea137fad84cbffad3fd7daabb8
Da <128560714+DavinaWorld@users.noreply.github.com>
2024-06-17 12:57:05 +0900

    Feat: Button 컴포넌트에서 작은 버튼을 위한  기본 사이즈 추가(수정필)

--- src/components/common/HeaderUI.tsx
+++ src/components/common/HeaderUI.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { Children, ReactNode } from "react";
 import RadixIcon from "@/assets/radix-icon";
 import { Button } from "../ui/Button";
 import Typography from "../ui/typograpy";
@@ -16,6 +16,7 @@ export const Header = ({ children }: HeaderProps) => {
   return <>{children}</>;
 };
 
+// * 로고
 const LogoAlert = () => {
   return (
     <nav className="flex justify-around sticky top-0 z-50 h-[60px] p-4 pt-2 bg-white shadow-nav-bar">
@@ -42,7 +43,7 @@ const LogoAlert = () => {
 
 const EditProfile = () => {
   return (
-    <nav className="flex justify-around sticky top-0 z-50 h-[60px] p-4 pt-2 bg-white shadow-nav-bar">
+    <nav className="flex justify-around sticky top-0 z-50 h-[60px] px-5 py-4 bg-white shadow-nav-bar">
       <div className="flex items-center justify-between w-[400]">
         <Typography type="h3" className="text-xl">
           프로필 편집
@@ -59,7 +60,7 @@ const EditProfile = () => {
 
 const LogoHamburgerButton = () => {
   return (
-    <nav className="flex justify-around sticky top-0 z-50 h-[60px] p-4 pt-2 bg-white shadow-nav-bar">
+    <nav className="flex justify-around sticky top-0 z-50 h-[60px] px-5 py-4 bg-white shadow-nav-bar">
       <div className="flex items-center justify-between w-full">
         <Typography type="h3" className="text-xl">
           iTIL
@@ -77,9 +78,12 @@ const LogoHamburgerButton = () => {
     </nav>
   );
 };
+
+// * 뒤로가기 화살표
+
 const PostSaveButtons = ({ onSave, onTempSave }: PostSaveProps) => {
   return (
-    <nav className="flex justify-around sticky top-0 z-50 h-[60px] p-4 py-2 bg-white shadow-nav-bar">
+    <nav className="flex justify-around sticky top-0 z-50 h-[60px] px-5 py-4 bg-white shadow-nav-bar">
       <div className="flex items-center justify-between w-full">
         <Typography type="h3" className="text-xl">
           <Button size={"icon"} variant={"ghost"}>
@@ -91,13 +95,34 @@ const PostSaveButtons = ({ onSave, onTempSave }: PostSaveProps) => {
           </Button>
         </Typography>
         <div className="flex gap-2">
-          <Button variant={"tempSave"} onClick={onTempSave}>
+          <Button variant={"tempSave"} size={"smButton"} onClick={onTempSave}>
             임시저장
           </Button>
-          <Button variant={"save"} onClick={onSave}>
+          <Button variant={"save"} size={"smButton"} onClick={onSave}>
             저장
           </Button>
         </div>
+      </div>
+    </nav>
+  );
+};
+const MessageHamburgerButton = (nickname: string) => {
+  return (
+    <nav className="flex justify-around sticky top-0 z-50 h-[60px] px-5 py-4 bg-white shadow-nav-bar">
+      <div className="flex items-center justify-between w-full">
+        <Typography type="h3" className="text-xl">
+          <Button size={"icon"} variant={"ghost"}>
+            <RadixIcon
+              name="ArrowLeftIcon"
+              size={20}
+              className=" -translate-y-0.5"
+            />
+          </Button>
+        </Typography>
+        <p>{nickname}</p>
+        <Button size={"icon"} variant={"ghost"}>
+          <RadixIcon name="RowsIcon" size={20} className=" -translate-y-0.5" />
+        </Button>
       </div>
     </nav>
   );
@@ -107,6 +132,7 @@ Header.Alert = LogoAlert;
 Header.HamburgerButton = LogoHamburgerButton;
 Header.EditProfile = EditProfile;
 Header.NewEditPost = PostSaveButtons;
+Header.Message = MessageHamburgerButton;
 
 //주석
 // export const MenubarUI = () => {
