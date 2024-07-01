d752895d1b66e9d59b5fde0f5ca7b70b00b946fe
hwangsungjae <edx2fire@likelion.org>
2024-07-01 21:14:59 +0900

    Feat: MenuBarUI 공통 컴포넌트화

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,6 +1,10 @@
-import { AppBarUI } from "@/components/common/bar/AppBarUI";
-import BottomNavBarUI from "@/components/common/bar/BottomNavBarUi";
-import { MainAppBar } from "@/components/common/bar/MainAppBar";
+import RadixIcon from '@/assets/radix-icon';
+import BottomNavBarUI from '@/components/common/bar/BottomNavBarUI';
+import { MenuBarUI } from '@/components/common/bar/MenuBarUI';
+import { Button } from '@/components/ui/Button';
+import Typography from '@/components/ui/typography';
+import { createClient } from '@/lib/supabase/server';
+import { getProfileById } from '@/services/profiles';
 
 export const dynamic = "force-dynamic";
 
@@ -9,10 +13,41 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    throw new Error(error?.message);
+  }
+  console.log(data);
+
+  // const profile = await getProfileById();
+  // console.log(profile);
+
   return (
     <>
-      <MainAppBar />
-      <div className="mb-16">{children}</div>
+      <MenuBarUI
+        left={
+          <Typography type='h3' className='text-xl'>
+            iTIL
+          </Typography>
+        }
+        right={
+          <div className='flex gap-1'>
+            <Button size={'icon'} variant={'ghost'}>
+              <RadixIcon name='BellIcon' size={20} />
+            </Button>
+            <Button size={'icon'} variant={'ghost'}>
+              <RadixIcon
+                name='PaperPlaneIcon'
+                size={20}
+                className='-rotate-45 -translate-y-0.5'
+              />
+            </Button>
+          </div>
+        }
+      />
+      {children}
       <BottomNavBarUI />
     </>
   );
--- src/components/common/bar/MenuBarUI.tsx
+++ src/components/common/bar/MenuBarUI.tsx
@@ -1,27 +1,17 @@
-import RadixIcon from '@/assets/radix-icon';
-import { Button } from '../../ui/Button';
-import Typography from '../../ui/typography';
+import { AppBarUI } from './AppBarUI';
 
-export const MenuBarUI = () => {
+interface IProps {
+  left?: React.ReactNode;
+  right?: React.ReactNode;
+  center?: React.ReactNode;
+}
+
+export const MenuBarUI = ({ left, right, center }: IProps) => {
   return (
-    <nav className='flex justify-around sticky top-0 z-50 h-[60px] p-4 pt-2 bg-white shadow-nav-bar'>
-      <div className='flex items-center justify-between w-full'>
-        <Typography type='h3' className='text-xl'>
-          iTIL
-        </Typography>
-        <div className='flex gap-1'>
-          <Button size={'icon'} variant={'ghost'}>
-            <RadixIcon name='BellIcon' size={20} />
-          </Button>
-          <Button size={'icon'} variant={'ghost'}>
-            <RadixIcon
-              name='PaperPlaneIcon'
-              size={20}
-              className='-rotate-45 -translate-y-0.5'
-            />
-          </Button>
-        </div>
-      </div>
-    </nav>
+    <AppBarUI>
+      {left && <AppBarUI.Left>{left}</AppBarUI.Left>}
+      {center && <AppBarUI.Center>{center}</AppBarUI.Center>}
+      {right && <AppBarUI.Right>{right}</AppBarUI.Right>}
+    </AppBarUI>
   );
 };
