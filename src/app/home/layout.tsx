f6ec71667015c75036fcf4121451d2834c17ae65
DavinaWorld <kdbj12003@gmail.com>
2024-07-03 20:23:04 +0900

    Chore:불필요한 컴포넌트  HeaderUI, PostModalUI 제거

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,30 +1,16 @@
-import { AppBarUI } from '@/components/common/bar/AppBarUI';
-import BottomNavBarUI from '@/components/common/bar/BottomNavBarUi';
-import { MainAppBar } from '@/components/common/bar/MainAppBar';
+import { AppBarUI } from "@/components/common/bar/AppBarUI";
+import BottomNavBarUI from "@/components/common/bar/BottomNavBarUi";
+import { MainAppBar } from "@/components/common/bar/MainAppBar";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const supabase = createClient();
-
-  // const { data, error } = await supabase.auth.getUser()
-  // if (error || !data?.user) {
-  //   throw new Error(error?.message)
-  // }
-
-  // const profile = await getProfileById();
-  // console.log(profile);
-
   return (
     <>
-      {/* <MenubarUI /> */}
-      {/* <Header>
-        <Header.Alert />
-      </Header> */}
       <MainAppBar />
       {children}
       <BottomNavBarUI />
